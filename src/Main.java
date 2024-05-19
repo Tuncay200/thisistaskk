bde02880e0289e10a247181063d35490105f18cc
Tuncay200 <tuncayadisirinli528@gmail.com>
2024-05-19 20:17:26 +0400

    fsafsa

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public  static void main(){
         int a=5;
         int b=3;
-        int temp=a+b;
-        System.out.println(temp);
+        int result=a+b;
+        System.out.println(result);
     }
 }
